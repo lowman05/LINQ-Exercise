aa386e4eaedacea6da44b62bdf54e88cf0aa9368
Daniel <dan.r.french@sbcglobal.net>
2023-05-31 11:05:48 -0500

    completed Linq-Exercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>
+            {
+                "Golden Axe", "Mortal Kombat", "Crazy Taxi", "Street Fighter"
+            };
+            IEnumerable<string> descendingVideoGames = videoGames.OrderByDescending(x=>x); 
+
+            foreach (var i in descendingVideoGames)
+            {
+                Console.WriteLine(i);
+            }
+
+            
+
+            
         }
     }
 }
